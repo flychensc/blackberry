e8231267e451d9811382a61a6e017e85944fb25c
flychensc <flychensc@163.com>
2022-02-08 22:08:52 +0800

    更细的分类

---
+++ washer.py
@@ -0,0 +1,106 @@
+from rqalpha.apis import *
+from scipy.optimize import leastsq
+
+import datetime as dt
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import configparser
+
+
+# refer to https://blog.csdn.net/qq_44158369/article/details/120205029
+def func(p, x):
+    k, b = p
+    return k * x + b
+
+
+def error(p, x, y):
+    return func(p, x) - y
+
+
+def classify(context, order_book_id, order_day, historys, disp=False):
+    Yi = historys['close']
+    Xi = np.sort(Yi)
+
+    #p0 = [Xi[0], Yi[0]]
+    p0 = [1, 1]
+
+    Para = leastsq(error, p0, args=(Xi, Yi))
+
+    k, b = Para[0]
+
+    if disp:
+        print(k, b)
+        plt.plot(Xi, Yi, color='green', linewidth=2)
+        plt.plot(Xi, k*Xi+b, color='red', linewidth=2)
+        plt.show()
+
+    k = round(k, 2)
+
+    if k > context.H2:
+        label = "PROFIT2"
+    elif k >= context.H1:
+        label = "PROFIT1"
+    elif k >= context.L1:
+        label = "HOLD"
+    elif k >= context.L2:
+        label = "LOSS1"
+    elif k >= context.L2:
+        label = "LOSS2"
+    else:
+        label = "LOSS3"
+
+    context.classifying.loc[(context.classifying['order_day'] == order_day) & (context.classifying['order_book_id'] == order_book_id), 'k'] = k
+    context.classifying.loc[(context.classifying['order_day'] == order_day) & (context.classifying['order_book_id'] == order_book_id), 'classify'] = label
+
+
+def init(context):
+    print(dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "START")
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+
+    context.BAR_COUNT = (dt.datetime.strptime(config.get("CLASSIFY", "DAY"), "%Y-%m-%d") - dt.datetime.strptime(config.get("PICK", "START_DAY"), "%Y-%m-%d")).days
+    context.BAR_COUNT = int(context.BAR_COUNT/7*5)
+    context.FREQUENCY = '1d'
+
+    context.POSITION_DAY = config.getint('POLICY', 'POSITION_DAY')
+    context.L3 = config.getfloat('POLICY', 'L3')
+    context.L2 = config.getfloat('POLICY', 'L2')
+    context.L1 = config.getfloat('POLICY', 'L1')
+    context.H1 = config.getfloat('POLICY', 'H1')
+    context.H2 = config.getfloat('POLICY', 'H2')
+
+    # context.classifying = pd.DataFrame(columns=['order_day','order_book_id', 'k', 'classify'])
+    context.classifying = pd.read_csv("picking.csv", parse_dates=["order_day"], date_parser=lambda x: dt.datetime.strptime(x, "%Y-%m-%d"))
+    # CONVERT dtype: datetime64[ns] to datetime.date
+    context.classifying['order_day'] = context.classifying['order_day'].dt.date
+
+    context.classifying = context.classifying.assign(profit=np.nan, classify="")
+
+
+def after_trading(context):
+    day = context.now.date()
+    stocks = all_instruments(type="CS")
+    for order_book_id in stocks['order_book_id']:
+        historys = history_bars(order_book_id, context.BAR_COUNT, context.FREQUENCY, fields=['datetime', 'close'], include_now=True)
+
+        if not historys.size: continue
+
+        order_data = context.classifying[(context.classifying['order_book_id'] == order_book_id) &
+                                     (context.classifying['order_day'] < day) &
+                                     (context.classifying['classify'] == "")]
+
+        for order_day in order_data['order_day'].sort_values():
+            order_day64 = np.int64(order_day.strftime("%Y%m%d%H%M%S"))
+            # 逐次缩小historys
+            historys = historys[(historys['datetime'] >= order_day64)]
+            # 数据不足
+            if historys.size < context.POSITION_DAY:
+                break
+            classify(context, order_book_id, order_day, historys[:context.POSITION_DAY])
+
+    if context.run_info.end_date == day:
+        context.classifying.to_csv('classifying.csv', index=False)
+        print(dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "END")
+
